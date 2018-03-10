aec48b4403551164a6609ec2e4ffcd142b237553
user-aacc-1 <36816768+user-aacc-1@users.noreply.github.com>
2018-03-10 17:31:04 +0600

    Create xxx.js

---
+++ xxx.js
@@ -0,0 +1,14 @@
+function str_rand() {
+        var result       = '';
+        var words        = '0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM';
+        var max_position = words.length - 1;
+            for( i = 0; i < 5; ++i ) {
+                position = Math.floor ( Math.random() * max_position );
+                result = result + words.substring(position, position + 1);
+            }
+        return result;
+    }
+$("#gen").click(function() {
+        $("#short_link").val(str_rand());       
+});
+
